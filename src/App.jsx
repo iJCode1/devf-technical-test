d3799917facfdce2ffa781d7bca32b1bfe2b3aa7
Joel <joeldome17@gmail.com>
2023-02-22 22:13:41 -0600

    initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hello">Hello world</div>
+    </div>
+  );
+}
+
+export default App;
